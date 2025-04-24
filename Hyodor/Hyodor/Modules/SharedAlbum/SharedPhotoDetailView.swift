cf6df38c9a58d678f68df53b3e461d93ae3f6db3
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2025-04-25 00:57:43 +0900

    [#1] 공유앨범 DetailView 이미지 캐싱 구현
    이미지 로드 시간 단축했습니다.

--- Hyodor/Hyodor/Modules/SharedAlbum/SharedPhotoDetailView.swift
+++ Hyodor/Hyodor/Modules/SharedAlbum/SharedPhotoDetailView.swift
@@ -9,15 +9,14 @@ import SwiftUI
 
 struct SharedPhotoDetailView: View {
     let photo: SharedPhoto
-    @State private var image: Image?
+    @State private var image: UIImage?
     @State private var isLoading = true
 
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-
             if let image = image {
-                image
+                Image(uiImage: image)
                     .resizable()
                     .scaledToFit()
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -28,20 +27,21 @@ struct SharedPhotoDetailView: View {
             }
         }
         .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-            loadImage()
-        }
+        .onAppear { loadImage() }
     }
 
     private func loadImage() {
-        guard let url = photo.imageURL else { return }
-        isLoading = true
+        guard let url = photo.imageURL else { isLoading = false; return }
+        if let cached = ImageCache.shared.get(forKey: url.absoluteString) {
+            self.image = cached
+            self.isLoading = false
+            return
+        }
         URLSession.shared.dataTask(with: url) { data, _, _ in
             isLoading = false
             if let data = data, let uiImage = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.image = Image(uiImage: uiImage)
-                }
+                ImageCache.shared.set(uiImage, forKey: url.absoluteString)
+                DispatchQueue.main.async { self.image = uiImage }
             }
         }.resume()
     }
