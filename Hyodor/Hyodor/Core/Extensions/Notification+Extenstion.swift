ef921d1e6faa88f9c49ae58953b72048aa612124
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2025-05-27 20:11:41 +0900

    [#15] AppDelegate 분리

---
+++ Hyodor/Hyodor/Core/Extensions/Notification+Extenstion.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Extenstion.swift
+//  Hyodor
+//
+//  Created by 김상준 on 5/27/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let newNotificationReceived = Notification.Name("newNotificationReceived")
+}
